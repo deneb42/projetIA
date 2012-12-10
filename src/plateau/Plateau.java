c24d37cab0c897676b4de6cd001692838a4b4c81
deneb42 <benjblois@hotmail.com>
2012-12-10 16:02:40 +0100

    niiiiiiiice design for the tab

--- src/plateau/Plateau.java
+++ src/plateau/Plateau.java
@@ -13,10 +13,28 @@ public class Plateau {
 								 };
 	
 	public Plateau() {
+		System.out.println(toString());
+	}
+	
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		str.append("┌─────────┐\n");
 		for(int i=0;i<grille.length;i++) {
+			str.append("│");
 			for(int j=0;j<grille[i].length;j++)
-				System.out.println(grille[i][j]);
-			System.out.println("");
+				switch(grille[i][j]) {
+				case -1: str.append("+"); break;
+				case 0: str.append("."); break;
+				case 1: str.append("○"); break;
+				case 2: str.append("●"); break;
+				}
+			str.append("│\n");
 		}
+		str.append("└─────────┘");
+		return str.toString();
+	}
+	
+	public static void main(String args[]) {
+		Plateau p = new Plateau();
 	}
 }
